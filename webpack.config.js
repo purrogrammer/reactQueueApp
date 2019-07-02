eebb82ea5ec8de418b18257ea9001566ab84cc74
Student <example@example.com>
2019-07-02 16:47:26 -0700

    create practice React app

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    resolve(__dirname, "src") + "/index.jsx"
+  ],
+
+  output: {
+    filename: 'app.bundle.js',
+    path: resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+
+  devtool: '#source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(_dirname, 'build'),
+    publicPath: '/'
+  },
+
+  module: {
+    rules: [
+       {
+        test:/\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ["es2015", {"modules": false}],
+            "react",
+          ],
+          plugins: [
+            "react-hot-loader/babel"
+          ]
+        }
+      },
+    ],
+  }
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ]
+};
